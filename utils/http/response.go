d177d00c09dff81bfbd59564b6b00aead1496ee0
akshanshgusain <akshanshgusain@gmail.com>
2022-07-08 12:09:10 +0530

    feat(utils/http/response): added all reponse functions

--- utils/http/response.go
+++ utils/http/response.go
@@ -1,1 +1,75 @@
 package http
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type response struct {
+	Status int         `json:"status"`
+	Result interface{} `json:"result"`
+}
+
+func newResponse(data interface{}, status int) *response {
+	return &response{
+		Status: status,
+		Result: data,
+	}
+}
+
+func (resp *response) bytes() []byte {
+	data, _ := json.Marshal(resp)
+	return data
+}
+
+func (resp *response) string() string {
+	return string(resp.bytes())
+}
+
+func (resp *response) sendResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(resp.Status)
+	_, _ = w.Write(resp.bytes())
+	log.Println(resp.string())
+}
+
+// StatusOK 200
+func StatusOK(w http.ResponseWriter, r *http.Request, data interface{}) {
+	newResponse(data, http.StatusOK).sendResponse(w, r)
+}
+
+// StatusNoContent 204
+func StatusNoContent(w http.ResponseWriter, r *http.Request) {
+	newResponse(nil, http.StatusNoContent).sendResponse(w, r)
+}
+
+// StatusBadRequest 400
+func StatusBadRequest(w http.ResponseWriter, r *http.Request, err error) {
+	data := map[string]interface{}{"error": err.Error()}
+	newResponse(data, http.StatusBadRequest).sendResponse(w, r)
+}
+
+// StatusNotFound 404
+func StatusNotFound(w http.ResponseWriter, r *http.Request, err error) {
+	data := map[string]interface{}{"error": err.Error()}
+	newResponse(data, http.StatusNotFound).sendResponse(w, r)
+}
+
+// StatusMethodNotAllowed 405
+func StatusMethodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	newResponse(nil, http.StatusMethodNotAllowed).sendResponse(w, r)
+}
+
+// StatusConflict 409
+func StatusConflict(w http.ResponseWriter, r *http.Request, err error) {
+	data := map[string]interface{}{"error": err.Error()}
+	newResponse(data, http.StatusConflict).sendResponse(w, r)
+}
+
+// StatusInternalServerError 500
+func StatusInternalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	data := map[string]interface{}{"error": err.Error()}
+	newResponse(data, http.StatusInternalServerError).sendResponse(w, r)
+}
