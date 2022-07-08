a6bacf89e332e6a5b2d4329233b1192775692c20
akshanshgusain <akshanshgusain@gmail.com>
2022-07-08 12:37:34 +0530

    feat(internal/repositiry/adapter/adapter): add functions to db interface

--- internal/repository/adapter/adapter.go
+++ internal/repository/adapter/adapter.go
@@ -13,6 +13,11 @@ type Database struct {
 }
 
 type Interface interface {
+	Health() bool
+	FindAll(condition expression.Expression, tableName string) (response *dynamodb.ScanOutput, err error)
+	FindOne(condition map[string]interface{}, tableName string) (response *dynamodb.GetItemOutput, err error)
+	CreateOrUpdate(entity interface{}, tableName string) (response *dynamodb.PutItemOutput, err error)
+	Delete(condition map[string]interface{}, tableName string) (response *dynamodb.DeleteItemOutput, err error)
 }
 
 func NewAdapter(con *dynamodb.DynamoDB) Interface {
