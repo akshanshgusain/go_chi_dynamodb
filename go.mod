2e80fae09a1b6e1ca93abfa34107e6e1038177bf
akshanshgusain <akshanshgusain@gmail.com>
2022-07-08 11:40:29 +0530

    feat(internal/routs/routes): added skaffold functions

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/akshanshgusain/Go-Chi-DynamoDB
 go 1.18
 
 require github.com/go-chi/cors v1.2.1
+
+require github.com/go-chi/chi/v5 v5.0.7 // indirect
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,5 +1,51 @@
 package routes
 
-func NewRouter() {
+import "github.com/go-chi/chi/v5"
+
+type Router struct {
+	config *Config
+	router *chi.Mux
+}
+
+func NewRouter() *Router {
+	return &Router{
+		config: NewConfig().SetTimeout(serviceConfig.GetConfig().Timeout),
+		router: chi.NewRouter(),
+	}
+}
+
+func (r *Router) SetRouter() *chi.Mux {
+
+}
+
+func (r *Router) setConfigRouters() {
+
+}
+
+func RouterHealth() {
+
+}
+
+func RouterProduct() {
+
+}
+
+func EnableTimeout() {
+
+}
+
+func EnableCORS() {
+
+}
+
+func EnableRecover() {
+
+}
+
+func EnableRequestId() {
+
+}
+
+func EnableRealIP() {
 
 }
